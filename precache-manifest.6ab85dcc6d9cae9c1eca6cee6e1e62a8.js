d9aadd2e113d824537a9451cff9c5bbbc470c41d
macycle <1124942097@qq.com>
2020-09-30 11:42:44 +0800

    修改路径

---
+++ precache-manifest.6ab85dcc6d9cae9c1eca6cee6e1e62a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccdeaf478008c012e8b873f76153a5d",
+    "url": "/money-trace-website/index.html"
+  },
+  {
+    "revision": "cd6f610dd935c93362ec",
+    "url": "/money-trace-website/static/css/main.7b3a3e77.chunk.css"
+  },
+  {
+    "revision": "c1623dd8a81fed50b822",
+    "url": "/money-trace-website/static/js/2.5cc50e4f.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/money-trace-website/static/js/2.5cc50e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6f610dd935c93362ec",
+    "url": "/money-trace-website/static/js/main.29e9aff4.chunk.js"
+  },
+  {
+    "revision": "7ccc7bfa98d2b8dd3df2",
+    "url": "/money-trace-website/static/js/runtime-main.2c726318.js"
+  }
+]);
